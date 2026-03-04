aaabe280676c01af016f72dabb0dd2673dfb89ca
“riskinurbarokah” <rizki.nurbarok4@gmail.com>
2026-03-04 15:34:59 +0700

    fix container setup

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,6 @@ WORKDIR /app
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY main.py .
+COPY . .
 
-CMD ["python", "main.py"]
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 --timeout 0 main:app
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-
+from flask import Flask, request
 from loguru import logger
 
+app = Flask(__name__)
 
-def hello_http(request):
+@app.route("/", methods=["GET", "POST"])
+def hello_http():
     try:
         logger.info("Starting ETL")
-
-
         return "✅ ETL Pipeline Completed"
-
     except Exception as e:
         logger.error(str(e))
         return f"❌ ETL FAILED: {str(e)}", 500
